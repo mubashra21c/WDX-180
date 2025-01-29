543401cd333c6a98b3e3f6ae036837b5612b0155
MUBASHRA IRSHAD <184015269+mubashra21c@users.noreply.github.com>
2025-01-29 16:00:39 +0000

    WEEK 6 DAY 4 EXERCISE UPDATED

--- curriculum/week06/exercises/hello-world.js
+++ curriculum/week06/exercises/hello-world.js
@@ -3,3 +3,23 @@ console.log("Hello world!");
 const userName = prompt("What is your name?");
 
 alert("Hello " + username + "!");
+
+
+/* Hello-world
+    This program concatenates hello with the input.
+
+
+  Data In:
+    enter username
+
+
+  Data Out:
+     concatenates hello with the useername.
+
+
+
+  Test Cases:
+     'mubashra'-> 'Hello mubashra!
+
+
+*/
