ce63021f9852de32f67a3481aab3a5db5bfe6adf
MUBASHRA IRSHAD <184015269+mubashra21c@users.noreply.github.com>
2025-03-13 20:06:03 +0000

    WEEK 9 DAY 5 EXERCISE UPDATED

---
+++ user/week09/exercises/day05/flexproperties.js
@@ -0,0 +1,27 @@
+const flexProperties={
+    container:{
+        display:{
+            flex:{},
+            "inline-flex":{}
+        },
+        "flex-direction":{
+            row:{"initial-value": true },
+            "row-reverse":{},
+            column:{},
+            "column-reverse":{}
+        },
+        "flex-wrap":{},
+        "justify-content":{},
+        "align-items":{},
+        "align-content":{}
+    },
+    children:{
+        order:{
+            integer:{}
+        },
+        "flex-grow":{},
+        "flex-basis":{},
+        "flex-shrink":{},
+        "align-self":{}
+    }
+}
